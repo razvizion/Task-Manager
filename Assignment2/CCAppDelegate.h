765a245f5ccc0d440e1cc2554ea16aed1e4462d7
razvizion <kolo111@gmail.com>
2014-03-19 00:37:56 +0100

    Initial Commit

---
+++ Assignment2/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Assignment2
+//
+//  Created by Michał Kozak on 19.03.2014.
+//  Copyright (c) 2014 Raz Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Assignment2/CCViewController.m
@@ -0,0 +1,29 @@
+//
+//  CCViewController.m
+//  Assignment2
+//
+//  Created by Michał Kozak on 19.03.2014.
+//  Copyright (c) 2014 Raz Labs. All rights reserved.
+//
+
+#import "CCViewController.h"
+
+@interface CCViewController ()
+
+@end
+
+@implementation CCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
